ae0201886345606d1f898dfdee2aa5d8566ffee3
Yashwanth Chowdary <160422550+mintu3770@users.noreply.github.com>
2026-01-30 01:12:24 +0530

    Remove download button event listener

--- popup.js
+++ popup.js
@@ -1,8 +1,1 @@
-document.getElementById("downloadBtn").addEventListener("click", () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.scripting.executeScript({
-      target: { tabId: tabs[0].id },
-      files: ["content.js"]
-    });
-  });
-});
+
